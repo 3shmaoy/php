345bd57e606ae081281d0b51b08956ed171183ff
amm <33675658+3shmaoy@users.noreply.github.com>
2025-04-15 13:28:33 +0900

    Update explode_convert.php

--- jp_multiByte/explode_convert.php
+++ jp_multiByte/explode_convert.php
@@ -1,9 +1,19 @@
 <?php
 
-//if ($bMatch) $output_array[0] = mb_convert_encoding($output_array[2], 'UTF-8', mb_list_encodings());
+// change encoding
+echo mb_convert_encoding('11：バックアップチェック', 'UTF-8', mb_list_encodings());
+echo PHP_EOL;
 
-echo mb_convert_kana('11：バックアップチェック', 'rnak') . "\n" ;
-$ary = mb_split('：', '11：バックアップチェック');
-var_export($ary);
+// convert to half width kana
+echo mb_convert_kana('11：バックアップチェック', 'rnak');
+echo PHP_EOL;
+
+// grep for multi-byte string
+var_export(mb_ereg('【指導】', '【指導】バックアップチェック'));
+echo PHP_EOL;
+
+// explode
+var_export(mb_split('：', '11：バックアップチェック'));
+echo PHP_EOL;
 
 ?>
